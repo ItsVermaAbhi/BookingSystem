e81684b7edba6043c12107a2925b1369c3c82714
Abhishek Verma <abhiverma227842@gmail.com>
2024-12-16 03:05:45 +0530

    added mongo model

--- backend/db.js
+++ backend/db.js
@@ -3,3 +3,82 @@ require('dotenv').config();
 
 
 mn.connect(process.env.MONGO_URL)
+
+const packageScema = mn.Schema({
+    title: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      availableDates: {
+        type: [Date], 
+        required: true,
+      },
+      image: {
+        type: String, 
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      updatedAt: {
+        type: Date,
+        default: Date.now,
+      }
+})
+
+const bookingInformationSchema = mn.Schema({
+    packageId: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Package',
+        required: true,
+      },
+      customerName: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      phoneNumber: {
+        type: String,
+        required: true,
+      },
+      numberOfTravelers: {
+        type: Number,
+        required: true,
+        min: [1, 'Number of travelers must be at least 1'],
+      },
+      specialRequests: {
+        type: String,
+        trim: true,
+        default: '',
+      },
+      totalPrice: {
+        type: Number,
+        required: true,
+      },
+      bookingDate: {
+        type: Date,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+})
+
+const Package = mn.model('Package', packageScema)
+const BookingInformation = mn.model('Booking', bookingInformationSchema)
+
+module.exports = {Package, BookingInformation}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// import userRoute from './user/index'
 const userRoute = require('./user/index')
 require('dotenv').config();
 
@@ -7,13 +6,6 @@ require('dotenv').config();
 const app = express()
 const PORT = 3000
 
-app.get('/', (req,res)=>{
-    return res.json({
-        msg: "heloo",
-        name: "abhi",
-        url: process.env.MONGO_URL
-    })
-})
 
 app.route('/user', userRoute)
 
